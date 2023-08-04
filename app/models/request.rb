eeebd0cb904ace6549683f7b01cbf1811a223932
william arasirwa <mageboarasirwa03@gmail.com>
2023-08-04 15:32:44 +0300

    requests update

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,7 +3,7 @@ class Request < ApplicationRecord
 
     validates :status, presence: true
     validates :user_id, presence: true
-    enum status: {pending: 0, accepted: 1, denied: 2}
+    enum status: {pending: 0, accepted: 1, denied: 2, done: 3}
 
 
 end
