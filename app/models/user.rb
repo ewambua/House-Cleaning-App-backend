01d85cf8ac599694d6fd1c523d9e1434ec6cdfb9
william arasirwa <mageboarasirwa03@gmail.com>
2023-08-07 10:04:16 +0300

    complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+    has_many :requests
+
     has_secure_password
 
     validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :plans
   resources :users
   resources :cleaners
+  resources :requests
   post '/login', to: 'authentication#login'
   post 'cleaner/login', to: 'authentication#cleaner_login'
   get '/*a', to: 'application#not_found'
