d35becec19c0f06ce59589de4d6799ece56f4bef
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-08-10 23:31:57 +0300

    add reviews to user backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,41 +2,40 @@ class UsersController < ApplicationController
 
     before_action :authorize_request, except: [:create, :show, :update]
     before_action :find_user, only: [:show, :update]
-  
+
     def index
       @users = User.all
       render json: @users, status: :ok
     end
-  
+
     def create
       @user = User.create!(user_params)
-  
+
       if @user.save
         render json: @user, status: :created
       else
         render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
       end
     end
-  
+
     def update
       unless @user.update(user_params)
         render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
       end
     end
-  
+
     def show
       render json: @user.slice(:id, :name, :username, :email, :created_at, :updated_at, :is_admin)
-                    .merge(requests: @user.requests) # Include associated requests
+                    .merge(requests: @user.requests, reviews: @user.reviews) # Include associated requests
     end
-  
+
     private
-  
+
     def find_user
       @user = User.find(params[:id])
     end
-  
+
     def user_params
       params.permit(:name, :username, :email, :password, :is_admin)
     end
   end
-  
